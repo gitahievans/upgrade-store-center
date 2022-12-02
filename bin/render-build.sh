ef79bbe579dac1c8e120036d3a7dfe6dad1230c8
gitahievans <gitahievance@gmail.com>
2022-12-02 17:16:22 +0300

    bin-render file changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,6 +9,5 @@ set -o errexit
 
 # Build commands for back end
 bundle install
-bundle exec rake assets:precompile
 bundle exec rake db:migrate 
 bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
